0467c6465c2ab0415e9e54e9d746b1093570165d
codingtoc <codingtoc@gmail.com>
2022-11-05 12:03:18 +0900

    App 컴포넌트에서 todos State 관리 및 TodosList 컴포넌트로 전달

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Container } from "@mui/material";
+import { useState } from "react";
+
 import NewTodo from "./components/NewTodo";
 import TodosList from "./components/TodosList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: "t1", text: "Learn React", isDone: true },
+    { id: "t2", text: "Learn TypeScript", isDone: false },
+  ]);
+
   return (
     <Container fixed>
       <NewTodo />
-      <TodosList />
+      <TodosList todos={todos} />
     </Container>
   );
 }
