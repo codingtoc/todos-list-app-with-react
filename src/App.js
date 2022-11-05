1ad309b47af68bffd44ad4e6367a2f4614e453a9
codingtoc <codingtoc@gmail.com>
2022-11-05 11:21:13 +0900

    App 컴포넌트의 최상위 컴포넌트를 Container 컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Fragment } from "react";
+import { Container } from "@mui/material";
 import NewTodo from "./components/NewTodo";
 import TodosList from "./components/TodosList";
 
 function App() {
   return (
-    <Fragment>
+    <Container fixed>
       <NewTodo />
       <TodosList />
-    </Fragment>
+    </Container>
   );
 }
 
