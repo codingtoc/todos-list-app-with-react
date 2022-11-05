ac868e90241f2202bd37e3981c7fd18309d75261
codingtoc <codingtoc@gmail.com>
2022-11-05 11:42:45 +0900

    NewTodo 컴포넌트 MUI를 이용하여 디자인

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,5 +1,20 @@
+import { Button, Paper, TextField, Typography } from "@mui/material";
+
 const NewTodo = () => {
-  return <div>NewTodo</div>;
+  return (
+    <Paper elevation={3} sx={{ margin: "10px 0", padding: "10px" }}>
+      <Typography variant="h5" component="div">
+        할일 추가
+      </Typography>
+      <TextField
+        label="신규 할일을 입력하세요."
+        variant="filled"
+        fullWidth
+        margin="normal"
+      />
+      <Button variant="contained">추가</Button>
+    </Paper>
+  );
 };
 
 export default NewTodo;
