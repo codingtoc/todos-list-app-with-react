bbf72f7f6b9fb1869b529907ede15140df7d1b47
codingtoc <codingtoc@gmail.com>
2022-11-06 12:06:22 +0900

    index.js 파일에서 App 컴포넌트를 TodosContextProvider로 감싸기

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { TodosContextPovider } from "./store/todos-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <TodosContextPovider>
+    <App />
+  </TodosContextPovider>
+);
