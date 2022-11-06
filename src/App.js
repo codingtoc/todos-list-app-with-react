5b02f70ff13e6e215a5fd828e00d955443f137fa
codingtoc <codingtoc@gmail.com>
2022-11-06 21:00:23 +0900

    HeaderAppBar 컴포넌트 구글로 로그인 및 로그아웃 기능 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import { Container } from "@mui/material";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import HeaderAppBar from "./components/HeaderAppBar";
 import NewTodo from "./components/NewTodo";
 import TodosList from "./components/TodosList";
+import { auth } from "./firebase-config";
+import { login, logout } from "./store/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(login(user.uid));
+      } else {
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Container fixed>
       <HeaderAppBar />
--- src/components/HeaderAppBar.js
+++ src/components/HeaderAppBar.js
@@ -1,6 +1,38 @@
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { getRedirectResult, signInWithRedirect, signOut } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+
+import { auth, googleAuthProvider } from "../firebase-config";
+import { login, logout } from "../store/authSlice";
 
 const HeaderAppBar = () => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const content = currentUser ? "Logout" : "Login with Google";
+
+  const toggleAuthClickHandler = () => {
+    if (currentUser) {
+      signOut(auth)
+        .then(() => {
+          dispatch(logout());
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } else {
+      signInWithRedirect(auth, googleAuthProvider)
+        .then((response) => {
+          return getRedirectResult(auth);
+        })
+        .then((result) => {
+          dispatch(login(result.user.uid));
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -8,7 +40,9 @@ const HeaderAppBar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Todos List App
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={toggleAuthClickHandler}>
+            {content}
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
