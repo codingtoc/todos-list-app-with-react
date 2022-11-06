7426f623fd26587a90635d78041071716fe4d5fd
codingtoc <codingtoc@gmail.com>
2022-11-06 15:37:28 +0900

    HeaderAppBar 컴포넌트 생성 및 App 컴포넌트에 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Container } from "@mui/material";
+
+import HeaderAppBar from "./components/HeaderAppBar";
 import NewTodo from "./components/NewTodo";
 import TodosList from "./components/TodosList";
 
 function App() {
   return (
     <Container fixed>
+      <HeaderAppBar />
       <NewTodo />
       <TodosList />
     </Container>
