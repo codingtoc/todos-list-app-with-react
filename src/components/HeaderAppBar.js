0535c0970509c90148ee6f0d31f5ac978f104aa6
codingtoc <codingtoc@gmail.com>
2022-11-06 16:02:28 +0900

    HeaderAppBar 컴포넌트 구글로 로그인 및 로그아웃 기능 적용

--- src/components/HeaderAppBar.js
+++ src/components/HeaderAppBar.js
@@ -1,6 +1,18 @@
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { useContext } from "react";
+import AuthContext from "../store/auth-context";
 
 const HeaderAppBar = () => {
+  const authContext = useContext(AuthContext);
+  const content = authContext.currentUser ? "Logout" : "Login with Google";
+  const toggleAuthClickHandler = () => {
+    if (authContext.currentUser) {
+      authContext.onLogout();
+    } else {
+      authContext.onLogin();
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -8,7 +20,9 @@ const HeaderAppBar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Todos List App
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={toggleAuthClickHandler}>
+            {content}
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
