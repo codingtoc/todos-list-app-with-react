41383c63e9b3bb0ba2020f79659e8c44f3f2e163
codingtoc <codingtoc@gmail.com>
2022-11-06 20:42:58 +0900

    authSlice 추가 및 Redux Store에 authReducer 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import todosReducer from "./todosSlice";
+import authReducer from "./authSlice";
 
 const store = configureStore({
   reducer: {
     todos: todosReducer,
+    auth: authReducer,
   },
 });
 
