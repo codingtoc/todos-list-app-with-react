04ce74d68c3054e3f4472403dcd7355f7dc24f34
codingtoc <codingtoc@gmail.com>
2022-11-06 12:47:39 +0900

    할일 수정 기능 적용

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,20 @@ import {
   CardActionArea,
   CardActions,
   CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
   Typography,
 } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import TodosContext from "../store/todos-context";
 
 const TodoItem = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [todoText, setTodoText] = useState(props.todo.text);
   const todosContext = useContext(TodosContext);
 
   const removeTodoClickHandler = () => {
@@ -19,6 +26,28 @@ const TodoItem = (props) => {
 
   const toggleTodoClickHandler = () => {
     todosContext.toggleTodo(props.todo.id, props.todo.isDone);
+  };
+
+  const dialogOpenClickHandler = () => {
+    setIsOpen(true);
+  };
+
+  const dialogCloseClickHandler = () => {
+    setIsOpen(false);
+    setTodoText(props.todo.text);
+  };
+
+  const todoTextChangeHandler = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  const updateTodoClickHandler = () => {
+    if (todoText.trim().length === 0) {
+      return;
+    }
+
+    todosContext.updateTodo(props.todo.id, todoText);
+    setIsOpen(false);
   };
 
   return (
@@ -35,7 +64,35 @@ const TodoItem = (props) => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button variant="contained">수정</Button>
+        <Button variant="contained" onClick={dialogOpenClickHandler}>
+          수정
+        </Button>
+        <Dialog open={isOpen} onClose={dialogCloseClickHandler}>
+          <DialogTitle>할일 수정</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              label="기존 할일을 수정하세요."
+              variant="standard"
+              fullWidth
+              margin="dense"
+              onChange={todoTextChangeHandler}
+              value={todoText}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={dialogCloseClickHandler}
+            >
+              취소
+            </Button>
+            <Button variant="contained" onClick={updateTodoClickHandler}>
+              수정
+            </Button>
+          </DialogActions>
+        </Dialog>
         <Button
           variant="contained"
           color="warning"
