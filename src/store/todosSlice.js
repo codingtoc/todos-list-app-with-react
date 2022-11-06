b854c61d5b321c9dbb4dcbdc016dd06b6c8a3c9d
codingtoc <codingtoc@gmail.com>
2022-11-06 16:48:33 +0900

    todos Redux State Slice 생성

---
+++ src/store/todosSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: Date.now().toString(),
+        text: action.payload,
+        isDone: false,
+      });
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todoIndex = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id
+      );
+      state.todos[todoIndex].isDone = !action.payload.isDone;
+    },
+    updateTodo: (state, action) => {
+      const todoIndex = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id
+      );
+      state.todos[todoIndex].text = action.payload.text;
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleTodo, updateTodo } =
+  todosSlice.actions;
+
+export default todosSlice.reducer;
