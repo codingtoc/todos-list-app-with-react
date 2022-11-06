af0dc72d67d0f7404d8c2ed3fdd1b20100fb4eab
codingtoc <codingtoc@gmail.com>
2022-11-06 18:19:58 +0900

    Firebase Realtime Database를 이용하여 할일 목록 조회

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,18 +1,40 @@
-import { List } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Alert, Box, CircularProgress, List } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { fetchTodos } from "../store/todosSlice";
 import TodoItem from "./TodoItem";
 
 const TodosList = () => {
-  const todos = useSelector((state) => state.todos.todos);
+  const { todos, isLoading, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-  return (
-    <List>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </List>
-  );
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  let content;
+  if (error) {
+    content = <Alert severity="error">{error}</Alert>;
+  } else if (isLoading) {
+    content = (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
+  } else if (todos.length > 0) {
+    content = (
+      <List>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </List>
+    );
+  } else {
+    content = <Alert severity="info">조회된 할일 목록이 없습니다.</Alert>;
+  }
+
+  return content;
 };
 
 export default TodosList;
--- src/store/todosSlice.js
+++ src/store/todosSlice.js
@@ -1,8 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   todos: [],
+  isLoading: false,
+  error: null,
 };
+
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+  const response = await fetch(
+    process.env.REACT_APP_REALTIMEDBURL + "todos.json"
+  );
+
+  if (!response.ok) {
+    throw new Error("할일을 추가하는데 실패했습니다.");
+  }
+
+  const data = await response.json();
+
+  const loadedTodos = [];
+  for (const key in data) {
+    loadedTodos.push({
+      id: key,
+      text: data[key].text,
+      isDone: data[key].isDone,
+    });
+  }
+
+  return loadedTodos;
+});
 
 const todosSlice = createSlice({
   name: "todos",
@@ -30,6 +55,20 @@ const todosSlice = createSlice({
       );
       state.todos[todoIndex].text = action.payload.text;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchTodos.pending, (state) => {
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchTodos.fulfilled, (state, action) => {
+      state.todos = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(fetchTodos.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.isLoading = false;
+    });
   },
 });
 
